32d8f459c6e5d37628c6b86014e92403a29ac5e5
Fabrice Rabaute <fabrice@arista.com>
2024-05-31 10:53:04 -0700

    Migrate from github.com/golang/mock to go.uber.org/mock

--- mock/gen.go
+++ mock/gen.go
@@ -3,8 +3,7 @@
 // that can be found in the COPYING file.
 
 // To generate the mock object, gomock and mockgen need to be installed, by running
-//    go get github.com/golang/mock/gomock
-//    go get github.com/golang/mock/mockgen
+//    go get go.uber.org/mock/mockgen@v0.4.0
 // then run 'go generate' to auto-generate mock.
 
 package mock
